ac9b2841d5c44c6a21a5bbd85e2dbab8fe8e6a9a
Julien Calixte <julienc@bam.tech>
2019-11-30 23:19:38 +0100

    :tada: (game) init game component

---
+++ src/components/AddGame.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="add-game">
+    <label for="game-id">Join a game</label>
+    <input type="text" id="game-id" name="game-id" v-model="gameId" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class AddGame extends Vue {
+  private gameId: string = ''
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Game.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="game"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Game extends Vue {
+  private gameId: string = ''
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
